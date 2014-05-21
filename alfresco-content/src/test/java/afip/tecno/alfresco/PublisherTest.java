d853c687652ca245c97ff48b2340b4e503a2af42
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2014-05-21 11:26:48 -0300

    Modifiqué el modelo para ajustarlo más al requerimiento. Modifiqué configuración de share para mostrar el nuevomodelo. Modifiqué publicador CMIS para adaptarlo al modelo.

--- alfresco-content/src/test/java/afip/tecno/alfresco/PublisherTest.java
+++ alfresco-content/src/test/java/afip/tecno/alfresco/PublisherTest.java
@@ -12,17 +12,29 @@ public class PublisherTest {
 		CMISPublisher publisher = new CMISPublisher();
 		Map<String, Object> props = new HashMap<String, Object>();
 
-		String targetPath = "/Sitios/prueba/documentLibrary/Adquisiciones";
+		String targetPath = "/Sitios/prueba/documentLibrary/Certificados";
 		ArrayList<String> aspects = new ArrayList<String>();
 
-		props.put(PropertyIds.OBJECT_TYPE_ID, "D:tecno:adquisicion");
+		props.put(PropertyIds.OBJECT_TYPE_ID, "D:tecno:form1900");
 		
-		props.put("tecno:responsable", "Pepe");
-		props.put("tecno:fechaAlta", new GregorianCalendar());
-
-		aspects.add("P:tecno:revisable");
+		GregorianCalendar today = new GregorianCalendar();
+		GregorianCalendar tomorrow = new GregorianCalendar();
+		tomorrow.add(GregorianCalendar.HOUR, 24);
 		
-		publisher.publish(targetPath, "Hola Mundo", "Hola mundo!!".getBytes(), props, aspects);
+		props.put("tecno:areaResponsable", "Comunicaciones");
+		props.put("tecno:direccion", "DITEC");
+		props.put("tecno:domicilio", "Paseo Colón 123");
+		props.put("tecno:expediente", "99/1234");
+		props.put("tecno:ordenCompra", 44);
+		props.put("tecno:fechaInicio", today);
+		props.put("tecno:fechaVencimiento", tomorrow);
+		props.put("tecno:objetoServicio", "No se");
+//		props.put("tecno:pdf", "");
+//		props.put("tecno:items", "");
+		
+		aspects.add("P:tecno:incumplido");
+		
+		publisher.publish(targetPath, "Prueba #1", "Prueba número 1".getBytes(), props, aspects);
 
 	}
 
