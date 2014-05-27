af91e6d6de7212b6344667a86821198992d1028b
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2014-05-27 09:16:35 -0300

    Implementación de un mecanismo de ratings como behavior custom de alfresco y un webscript para probarlo (además de los unit tests)

---
+++ alfresco-poc-repo/src/main/java/afip/tecno/alfresco/model/TecnoRatingsModel.java
@@ -0,0 +1,22 @@
+package afip.tecno.alfresco.model;
+
+public class TecnoRatingsModel {
+	// Namespaces
+	public static final String NAMESPACE_TECNO_RATINGS_CONTENT_MODEL = "http://www.afip.gob.ar/model/ratings/1.0";
+
+	// Types
+	public static final String TYPE_SCR_RATING = "rating";
+
+	// Aspects
+	public static final String ASPECT_SCR_RATEABLE = "rateable";
+
+	// Properties
+	public static final String PROP_RATING = "rating";
+	public static final String PROP_RATER = "rater";
+	public static final String PROP_AVERAGE_RATING = "averageRating";
+	public static final String PROP_TOTAL_RATING = "totalRating";
+	public static final String PROP_RATING_COUNT = "ratingCount";
+
+	// Associations
+	public static final String ASSN_SCR_RATINGS = "ratings";
+}
